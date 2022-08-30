5e53dfd6d6d808b14efef208ccf4c7379dd6001e
rayyanhammadi <rayyan.hammedi@gmail.com>
2022-08-30 10:53:15 +0100

    update: testing backtester

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # trading_bot
 Implementation of a trading bot based on momentum indicators
+
+credentials : HMAC_SHA256 Key registered ; 
+Logged in as test user 1661426907660578150
+
+
+API Key: lbrCzGsrBGwdhba84A9hNBmBLyLdKjrF0f45BXw36i4J3qN2gMZ9UyweigUrhIlF
+
+Secret Key: 84759Gd37HRCuQhSbmTpkyYzECdvQ6auGcKZ8HDwQFH3aqSHrpBpsc5jJLgb1Yv0
--- test.py
+++ test.py
@@ -2,20 +2,26 @@ try:
     import pandas as pd
     from service.binance_service import BinanceService
     from strategies.strategy import Strategy
+    from backtests.backtester import Backtester
     from backtests.historical_data import HistoricalData
+    from service.data_provider_service import Data
     from utils.helpers import TechnicalAnalysis
-    from utils.consts import INTERVAL, SYMBOLS, TA_INDICATORS
+    from utils.consts import INTERVAL, SYMBOLS, INDICATORS
+    from datetime import datetime
 except Exception as e:
     raise e
 
 
-indicators = {'trend': {'indicator1': {'name': TA_INDICATORS.EMA50, 'params': None}},
-              'momentum': {'indicator1': {'name': TA_INDICATORS.RSI, 'params': {"window" : 14}}}}
+indicators = {'trend': {'indicator1': {'name': INDICATORS.EMA50, 'params': None}},
+              'momentum': {'indicator1': {'name': INDICATORS.RSI, 'params': {"window" : 14}}}}
 strategy = Strategy("bu baker","RSI + BB", INTERVAL.ONE_MONTH)
 strategy.tradable_coins = [SYMBOLS.BTCUSDT, SYMBOLS.ETHUSDT]
 strategy.indicators = indicators
 client = BinanceService(key="lbrCzGsrBGwdhba84A9hNBmBLyLdKjrF0f45BXw36i4J3qN2gMZ9UyweigUrhIlF",
                         secret="84759Gd37HRCuQhSbmTpkyYzECdvQ6auGcKZ8HDwQFH3aqSHrpBpsc5jJLgb1Yv0")
-historical_data = HistoricalData(client.get_historical_raw_data(SYMBOLS.ETHUSDT, INTERVAL.ONE_WEEK, "1 JANUARY 2020"))
-# print(historical_data.df)
-print(historical_data.prepare_backtesting_data(strategy=strategy))
+data = Data(client=client, symbol=SYMBOLS.ETHUSDT, interval=INTERVAL.ONE_WEEK, start_date="1 JANUARY 2020")
+historical_data = HistoricalData(data)
+backtester = Backtester(1000, strategy=strategy, hist_data=historical_data)
+
+backtester.start_backtest()
+#todo : test backtest (thx for playing)
