f24111f7b149332a4389479f48bd8904fd2b8662
rayyanhammadi <rayyan.hammedi@gmail.com>
2022-08-30 10:51:42 +0100

    Update binance_service

--- service/binance_service.py
+++ service/binance_service.py
@@ -1,6 +1,5 @@
 """binance service"""
 try:
-    import pandas as pd
     from binance import Client
     from utils.consts import INTERVAL
 except Exception as e:
@@ -14,14 +13,6 @@ class BinanceService:
         """constructor"""
         self.client = Client(api_key=key, api_secret=secret)
 
-    def get_historical_raw_data(self, symbol, interval: str, start_date: str, end_date: str = None):
-        df = pd.DataFrame(self.client.get_historical_klines(symbol=symbol, interval=interval,
-                                                            start_str=start_date, end_str=end_date),
-                          columns=['timestamp', 'open', 'high', 'low',
-                                   'close', 'volume', 'close_time', 'quote_av',
-                                   'trades', 'tb_base_av', 'tb_quote_av', 'ignore'],
-                          )
-        df.drop(columns=df.columns.difference(['timestamp','open', 'high', 'low', 'close', 'volume']), inplace=True)
-
-        return df
+    def get_historical_klines(self,symbol, interval, start_str, end_str):
+        return self.client.get_historical_klines(symbol=symbol, interval=interval, start_str=start_str, end_str=end_str)
 
